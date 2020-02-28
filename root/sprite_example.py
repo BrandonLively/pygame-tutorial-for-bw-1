be85e00a14a29bad59e73144a23b3fa51b8d0f20
BrandonLively <akutenshi@live.com>
2020-02-28 05:09:56 -0500

    Setup sprite_example - Added temp player class

--- root/sprite_example.py
+++ root/sprite_example.py
@@ -1,3 +1,4 @@
+# Pygame template - skeleton for a new pygame project
 import pygame
 import random
 
@@ -12,17 +13,27 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-
-#initialize Pygame and create window
+# initialize pygame and create window
 pygame.init()
-# allows sound
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
-# allows constant fps
 clock = pygame.time.Clock()
 
-# Game Loop
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH // 2, HEIGHT // 2)
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+# Game loop
 running = True
 while running:
     # keep loop running at the right speed
@@ -32,10 +43,14 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+
     # Update
+    all_sprites.update(screen)
+
     # Draw / render
     screen.fill(BLACK)
-    # sets up double buffering - always doing this After drawing everything
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
     pygame.display.flip()
 
 pygame.quit()
