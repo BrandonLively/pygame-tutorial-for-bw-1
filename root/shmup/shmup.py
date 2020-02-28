6d9a3d7f21c2888d73ff7bd4239b50c45e1f28da
BrandonLively <akutenshi@live.com>
2020-02-28 08:13:01 -0500

    added health bar which takes damage depending on the size of the meteor hit

--- root/shmup/shmup.py
+++ root/shmup/shmup.py
@@ -39,6 +39,22 @@ def draw_text(surf, text, size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
+def draw_shield_bar(surface, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct/100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surface, GREEN, fill_rect)
+    pygame.draw.rect(surface, WHITE, outline_rect, 2)
+
+def new_mob():
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -51,6 +67,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH // 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.shield = 100
 
     def update(self):
         self.speedx = 0
@@ -175,14 +192,13 @@ player = Player()
 all_sprites.add(player)
 
 for i in range(8):
-    m = Mob()
-    all_sprites.add(m)
-    mobs.add(m)
+    new_mob()
 
 score = 0
-pygame.mixer.music.play(loops = -1)
+pygame.mixer.music.play(loops=-1)
 # Game Loop
 running = True
+
 while running:
     # keep loop running at the right speed
     clock.tick(FPS)
@@ -202,18 +218,20 @@ while running:
     for hit in hits:
         score += 70 - hit.radius
         random.choice(expl_sounds).play()
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
+        new_mob()
     # check to see if mob hit player
-    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
-    if hits:
-        running = False
+    hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        player.shield -= hit.radius
+        new_mob()
+        if player.shield <= 0:
+            running = False
     # Draw / render
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
-    draw_text(screen, str(score), 18, WIDTH//2, 10)
+    draw_text(screen, str(score), 18, WIDTH // 2, 10)
+    draw_shield_bar(screen, 5, 5, player.shield)
     # sets up double buffering - always doing this After drawing everything
     pygame.display.flip()
 
